8c5f2c78c1f9bd20001e5c67e8a6a5f70cc888ed
ongzhixian <zhixian@hotmail.com>
2026-09-12 22:22:09 +0800

    Sample hello using modules

--- sample-hello/README.md
+++ sample-hello/README.md
@@ -1,3 +1,11 @@
 # sample-hello
 
 This is a sample hello-world project use for learning / testing out Go.
+
+
+## Referencing other modules
+
+Use this command:
+```cli
+go work use ../sample-module1
+```
--- sample-hello/main.go
+++ sample-hello/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sample/module1/greetings"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	message := greetings.Hello("Zhixian")
+	fmt.Println(message)
 }
